1c30e4e18447fb3ee03e54eb5c255bfe9aa4d047
Meng Li <Mengli619@github.com>
2020-11-05 00:22:48 +0800

    update readme

--- install-xorg-service.sh
+++ install-xorg-service.sh
@@ -7,7 +7,7 @@ apt-get install -y xserver-xorg-core
 # Create xorg.conf
 BUS_ID=$(nvidia-xconfig --query-gpu-info | grep BusID | awk '{print $4}')
 echo "Nvidia bus id is $BUS_ID"
-nvidia-xconfig nvidia-xconfig --virtual=1920x1200 --busid "$BUS_ID"
+nvidia-xconfig --virtual=1920x1200 --busid "$BUS_ID"
 
 # Create xorg service to keep it always running
 cat > /usr/lib/systemd/system/xorg.service << EOF
@@ -28,4 +28,4 @@ systemctl daemon-reload
 systemctl start xorg
 systemctl enable xorg
 
-echo "xorg service started and enabled, use 'systemctl status xorg to see the status'"
+echo "xorg service started and enabled, run 'systemctl status xorg' to see the status"
